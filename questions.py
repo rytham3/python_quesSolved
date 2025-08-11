039a15221b32e6cc8b94d63db8a761f14236132e
Rytham B. <123790286+rytham3@users.noreply.github.com>
2025-08-11 23:24:23 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,25 @@
+# addition of 2 numbers
+a = 10
+b = 5
+
+c = a+b
+# print(c)
+
+
+
+# multiplication of 2 numbers
+a = 10
+b = 99
+
+d = a*b
+# print("Your answer is:",d)
+
+
+
+
+# division of 2 numbers
+a = 12
+b = 6
+
+c1 = a/b
+#print("Your answer is: ",c1)
